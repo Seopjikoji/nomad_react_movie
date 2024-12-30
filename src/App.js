ef84b9fe72d8333a39dfecb8aedc48bb4ff6b421
ctensoreo <ctensoreo@gmail.com>
2024-12-30 14:14:48 +0900

    feat: cleanup 함수 실습

--- src/App.js
+++ src/App.js
@@ -1,45 +1,35 @@
 import { useEffect, useState } from "react";
 
+function Hello(){
+
+  function CleanUpFn(){
+    console.log("Destroyed :(")  
+  }
+
+  function EffectFn(){
+    console.log("Created :)")
+    return CleanUpFn;
+  }
+
+  function CleanUpFn(){
+    console.log("Destroyed :(")  
+  }
+
+  useEffect(EffectFn,[])
+    
+  return(
+    <h1>Hello</h1>
+)
+
+}
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const onClick = () => setCounter((prev) => prev + 1)
-  const onChange = (event) => setKeyword(event.target.value);
-
-  // useEffect(() => {
-  //   console.log("CALL THE API ...");
-  // }, [])
-
-  // console.log("I run all the time")
-
-  // useEffect(()=>{
-  //   if(keyword !== "" && keyword.length >5){
-  //     console.log("SEARCH FOR", keyword)
-  //   }
-  // }, [keyword])
-
-  useEffect(()=>{
-    console.log("I run only once.")
-  }, [])
-
-  useEffect(()=>{
-    console.log("I run when 'keyword' changes.")
-  }, [keyword])
-
-  useEffect(()=>{
-    console.log("I run when 'counter' changes.")
-  }, [counter])
-
-  useEffect(()=>{
-    console.log("I run when 'keyword' & 'counter' changes.")
-  }, [keyword, counter])
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev)=> !prev)
 
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search Here !" />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click</button>
+      {showing ? <Hello/> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
